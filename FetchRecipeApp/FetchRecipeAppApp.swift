00e52284e0abf1084e862a081a5ee11320632f8c
Mustafa Mohammed <dev.mustafa.t.mohammed@gmail.com>
2025-01-27 09:34:17 -0500

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Mustafa T Mohammed on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
